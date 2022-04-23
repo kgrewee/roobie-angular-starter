f76bff6875eb90e1258b61eb7e7eb3d0cd056388
Kyle Grewe <kyle.grewe@caci.com>
2022-04-23 19:30:16 -0400

    Update to roobie 0.4.6, add more examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 declare var Tooltip;
 declare var Accordion;
+declare var Carousel;
+declare var Navbar;
+declare var Select;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'roobie-angular-starter';
   constructor() {
+  }
+  ngAfterViewInit(): void {
     Tooltip.check();
     Accordion.check();
+    Carousel.check();
+    Navbar.check();
+    Select.check();
   }
 }
 
